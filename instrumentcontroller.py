10fa32e2477ffa24be7fd9ed8bcb14647104d1b7
igrekus <upload234@mail.ru>
2020-11-01 15:52:19 +0300

    Initial commit

---
+++ instrumentcontroller.py
@@ -0,0 +1,193 @@
+import time
+
+from os.path import isfile
+from PyQt5.QtCore import QObject, pyqtSlot
+
+from arduino.programmerfactory import ProgrammerFactory
+from instr.instrumentfactory import NetworkAnalyzerFactory, SourceFactory, mock_enabled
+from measureresult import MeasureResult
+
+
+class InstrumentController(QObject):
+    states = {
+        i * 5.625: i for i in range(64)
+    }
+
+    main_states = [0, 1, 2, 4, 8, 16, 32, 63]
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        self.requiredInstruments = {
+            'Анализатор': NetworkAnalyzerFactory('GPIB0::9::INSTR'),
+            'Источник питания': SourceFactory('GPIB0::5::INSTR'),
+            'Программатор': ProgrammerFactory('COM5')
+        }
+
+        self.deviceParams = {
+            'Цифровой аттенюатор': {
+                'F': [1.15, 1.35, 1.75, 1.92, 2.25, 2.54, 2.7, 3, 3.47, 3.86, 4.25],
+                'mul': 2,
+                'P1': 15,
+                'P2': 21,
+                'Istat': [None, None, None],
+                'Idyn': [None, None, None]
+            },
+        }
+
+        if isfile('./params.ini'):
+            import ast
+            with open('./params.ini', 'rt', encoding='utf-8') as f:
+                raw = ''.join(f.readlines())
+                self.deviceParams = ast.literal_eval(raw)
+
+        self.secondaryParams = {
+            'Pin': -10,
+            'F1': 4,
+            'F2': 8,
+            'kp': 0,
+            'Fborder1': 4,
+            'Fborder2': 8
+        }
+
+        self.sweep_points = 201
+        self.cal_set = 'Upr_tst'
+
+        self._instruments = dict()
+        self.found = False
+        self.present = False
+        self.hasResult = False
+        self.only_main_states = False
+
+        self.result = MeasureResult()
+
+        self._freqs = list()
+        self._mag_s11s = list()
+        self._mag_s22s = list()
+        self._mag_s21s = list()
+        self._phs_s21s = list()
+        self._phase_values = list()
+        self._current = 0
+
+    def __str__(self):
+        return f'{self._instruments}'
+
+    def connect(self, addrs):
+        print(f'searching for {addrs}')
+        for k, v in addrs.items():
+            self.requiredInstruments[k].addr = v
+        self.found = self._find()
+
+    def _find(self):
+        self._instruments = {
+            k: v.find() for k, v in self.requiredInstruments.items()
+        }
+        return all(self._instruments.values())
+
+    def check(self, params):
+        print(f'call check with {params}')
+        device, secondary = params
+        self.present = self._check(device, secondary)
+        print('sample pass')
+
+    def _check(self, device, secondary):
+        print(f'launch check with {self.deviceParams[device]} {self.secondaryParams}')
+        return self._runCheck(self.deviceParams[device], self.secondaryParams)
+
+    def _runCheck(self, param, secondary):
+        print(f'run check with {param}, {secondary}')
+        return True
+
+    def measure(self, params):
+        print(f'call measure with {params}')
+        device, secondary = params
+        self.result.raw_data = self.sweep_points, self._measure(device, secondary), self._phase_values, self.secondaryParams, self._current
+        self.hasResult = bool(self.result)
+
+    def _measure(self, device, secondary):
+        param = self.deviceParams[device]
+        secondary = self.secondaryParams
+        print(f'launch measure with {param} {secondary}')
+
+        self._clear()
+        self._init(secondary)
+
+        return self._measure_s_params()
+
+    def _clear(self):
+        self._phase_values.clear()
+
+    def _init(self, params):
+        pna = self._instruments['Анализатор']
+        prog = self._instruments['Программатор']
+
+        pna.send('SYST:PRES')
+        pna.query('*OPC?')
+        # pna.send('SENS1:CORR ON')
+
+        pna.send('CALC1:PAR:DEF "CH1_S21",S21')
+
+        # c:\program files\agilent\newtowrk analyzer\UserCalSets
+        pna.send(f'SENS1:CORR:CSET:ACT "{self.cal_set}",1')
+        # pna.send('SENS2:CORR:CSET:ACT "-20dBm_1.1-1.4G",1')
+
+        pna.send(f'SENS1:SWE:POIN {self.sweep_points}')
+
+        pna.send(f'SENS1:FREQ:STAR {params["F1"]}GHz')
+        pna.send(f'SENS1:FREQ:STOP {params["F2"]}GHz')
+
+        pna.send('SENS1:SWE:MODE CONT')
+        pna.send(f'FORM:DATA ASCII')
+
+        prog.set_lpf_code(0)
+
+    def _measure_s_params(self):
+        pna = self._instruments['Анализатор']
+        prog = self._instruments['Программатор']
+
+        self._current = 0.01
+
+        out = []
+        for deg, code in self.states.items():
+            if self.only_main_states and code not in self.main_states:
+                continue
+            self._phase_values.append(deg)
+
+            prog.set_lpf_code(code)
+
+            if not mock_enabled:
+                time.sleep(0.5)
+
+            pna.send(f'CALC1:PAR:SEL "CH1_S21"')
+            pna.query('*OPC?')
+            res = pna.query(f'CALC1:DATA:SNP? 2')
+
+            pna.send(f'CALC:DATA:SNP:PORTs:Save "1,2", "d:/ksa/psm/s{code}.s2p"')
+            pna.send(f'MMEM:STOR "d:/ksa/psm1/s{code}.s2p"')
+
+            # with open(f's2p_{code}.s2p', mode='wt', encoding='utf-8') as f:
+            #     f.write(res)
+            if mock_enabled:
+                with open(f'ref/sample_data/s2p_{code}.s2p', mode='rt', encoding='utf-8') as f:
+                    res = list(f.readlines())[0].strip()
+            out.append(parse_float_list(res))
+
+            if not mock_enabled:
+                time.sleep(0.5)
+        return out
+
+    def pow_sweep(self):
+        print('pow sweep')
+        return [4, 5, 6], [4, 5, 6]
+
+    @pyqtSlot(dict)
+    def on_secondary_changed(self, params):
+        self.secondaryParams = params
+
+    @property
+    def status(self):
+        return [i.status for i in self._instruments.values()]
+
+
+def parse_float_list(lst):
+    return [float(x) for x in lst.split(',')]
---
+++ measureresult.py
@@ -0,0 +1,436 @@
+import itertools
+import math
+import random
+import statistics
+
+
+def unwrap(xw):
+    dist = 180
+    xu = list(xw)
+    for i in range(1, len(xw)):
+        diff = xw[i] - xw[i - 1]
+        if diff > dist:
+            for j in range(i, len(xu)):
+                xu[j] -= 2 * dist
+        elif diff < -dist:
+            for j in range(i, len(xu)):
+                xu[j] += 2 * dist
+    return xu
+
+
+def calc_vswr(in_mags: list):
+    temp = map(lambda x: x / 20, in_mags)
+    modulated = list(map(lambda x: pow(10, x), temp))
+    plus = map(lambda x: 1 + x, modulated)
+    minus = map(lambda x: 1 - x, modulated)
+    out = map(lambda x: x[0] / x[1], zip(plus, minus))
+    return list(out)
+
+
+def calc_error(array, zero):
+    return [a - z for a, z in zip(array, zero)]
+
+
+def calc_phase_error(array, zero, ideal):
+    return [a - z - ideal if (a - z - ideal) > -200 else (a - z - ideal + 360) for a, z in zip(array, zero)]
+
+
+def calc_rmse_phase(values, mean):
+    return math.sqrt(sum(pow(v, 2) for v in values) / len(values))
+
+
+def calc_rmse_amp(values, mean):
+    return math.sqrt(sum(pow(v, 2) for v in values) / len(values))
+
+
+def shift_vals(values, shift):
+    return [s + shift for s in values]
+
+
+def mul_vals(values, shift):
+    return [s * shift for s in values]
+
+
+def generateValue(data):
+    span, step, mean = data
+    start = mean - span
+    stop = mean + span
+    return round(random.randint(0, int((stop - start) / step)) * step + start, 2)
+
+
+def _find_freq_index(freqs: list, freq):
+    freq = freq * 1_000_000_000
+    return min(range(len(freqs)), key=lambda i: abs(freqs[i] - freq))
+
+
+class MeasureResult:
+    adjust_dirs = {
+        1: 'data/+25',
+        2: 'data/+85',
+        3: 'data/-60',
+    }
+
+    main_states = [0, 1, 2, 4, 8, 16, 32, 63]
+
+    def __init__(self, ):
+
+        self.headers = list()
+        self._secondaryParams = dict()
+
+        self._freqs = list()
+        self._s21s = list()
+        self._s21s_err = list()
+        self._s21s_rmse = list()
+        self._s21s_ph = list()
+        self._s21s_ph_err = list()
+        self._s21s_ph_rmse = list()
+        self._s11s = list()
+        self._s22s = list()
+        self._ideal_phase = list()
+
+        self._vswr_in = list()
+        self._vswr_out = list()
+
+        self._s21_mins = list()
+        self._vswr_in_max = list()
+        self._vswr_out_max = list()
+        self._phase_rmse_values = list()
+        self._s21_rmse_values = list()
+        self._phase_err_max = list()
+        self._s21_err_max = list()
+
+        self._misc = list()
+
+        self._kp_freq_min = 0
+        self._kp_freq_max = 0
+
+        self._min_freq_index = 0
+        self._max_freq_index = 0
+
+        self.adjust = False
+        self.only_main_states = False
+        self._adjust_dir = self.adjust_dirs[1]
+        self.ready = False
+
+    def __bool__(self):
+        return self.ready
+
+    def _init(self):
+        self._secondaryParams.clear()
+        self._freqs.clear()
+        self._s21s.clear()
+        self._s21s_err.clear()
+        self._s21s_rmse.clear()
+        self._s21s_ph.clear()
+        self._s21s_ph_err.clear()
+        self._s21s_ph_rmse.clear()
+        self._s11s.clear()
+        self._s22s.clear()
+        self._ideal_phase.clear()
+
+        self._vswr_in.clear()
+        self._vswr_out.clear()
+
+        self._s21_mins.clear()
+        self._vswr_in_max.clear()
+        self._vswr_out_max.clear()
+        self._phase_rmse_values.clear()
+        self._s21_rmse_values.clear()
+        self._phase_err_max.clear()
+        self._s21_err_max.clear()
+
+        self._kp_freq_min = 0
+        self._kp_freq_max = 0
+
+        self._current = 0
+
+        self._misc.clear()
+
+    def _process(self):
+        if self.adjust:
+            self._adjust_data('s21')
+        self._calc_vwsr_in()
+        self._calc_vwsr_out()
+        if self.adjust:
+            self._adjust_data('vswr')
+        self._calc_phase_err()
+        self._calc_s21_err()
+        if self.adjust:
+            self._adjust_data('err')
+        self._calc_phase_rmse()
+        self._calc_s21_rmse()
+        self._calc_stats()
+
+        self._cal_s21_worst_loss()
+
+        self.ready = True
+
+    def _calc_vwsr_in(self):
+        self._vswr_in = [calc_vswr(s) for s in self._s11s]
+
+    def _calc_vwsr_out(self):
+        self._vswr_out = [calc_vswr(s) for s in self._s22s]
+
+    def _calc_phase_err(self):
+        self._s21s_ph = [unwrap(s) for s in self._s21s_ph]
+        ph0 = self._s21s_ph[0]
+        self._s21s_ph_err = [calc_phase_error(s, ph0, ideal) for s, ideal in zip(self._s21s_ph[1:], self._ideal_phase[1:])]
+
+        means = [statistics.mean(vs) for vs in zip(*self._s21s_ph_err)]
+
+        self._s21s_ph_err = [calc_error(s, mean) for s, mean in zip(self._s21s_ph_err, itertools.repeat(means, len(self._s21s_ph_err)))]
+
+    def _calc_s21_err(self):
+        means = [statistics.mean(vs) for vs in zip(*self._s21s)]
+        self._s21s_err = [calc_error(s, means) for s in self._s21s]
+
+    def _calc_phase_rmse(self):
+        means = [statistics.mean(vs) for vs in zip(*self._s21s_ph_err)]
+        for *vs, mean in zip(*self._s21s_ph_err, means):
+            self._s21s_ph_rmse.append(calc_rmse_phase(vs, mean))
+
+    def _calc_s21_rmse(self):
+        means = [statistics.mean(vs) for vs in zip(*self._s21s)]
+        for *vs, mean in zip(*self._s21s_err, means):
+            self._s21s_rmse.append(calc_rmse_amp(vs, mean))
+
+    def _adjust_data(self, what):
+        if what == 'err':
+            err_mul = random.uniform(0.875, 1.125)
+            self._s21s_err = [mul_vals(s, err_mul) for s in self._s21s_err]
+            self._s21s_ph_err = [mul_vals(s, err_mul) for s in self._s21s_ph_err]
+        elif what == 's21':
+            s21_shift = random.uniform(-0.2, 0.2)
+            self._s21s = [shift_vals(s, s21_shift) for s in self._s21s]
+        elif what == 'vswr':
+            vswr_in_shift = random.uniform(-0.05, 0.05)
+            vswr_out_shift = random.uniform(-0.05, 0.05)
+            self._vswr_in = [shift_vals(s, vswr_in_shift) for s in self._vswr_in]
+            self._vswr_out = [shift_vals(s, vswr_out_shift) for s in self._vswr_out]
+        else:
+            return
+
+    def _calc_stats(self):
+        self._min_freq_index = _find_freq_index(self._freqs, self._secondaryParams['Fborder1'])
+        self._max_freq_index = _find_freq_index(self._freqs, self._secondaryParams['Fborder2'])
+
+        mid = self._min_freq_index + abs(self._max_freq_index - self._min_freq_index) // 2
+
+        vs = list(zip(*self.s21))
+        self._s21_mins = [min(vs[self._min_freq_index]), min(vs[mid]), min(vs[self._max_freq_index])]
+
+        vs = list(zip(*self.vswr_in))
+        self._vswr_in_max = [max(vs[self._min_freq_index]), max(vs[mid]), max(vs[self._max_freq_index])]
+
+        vs = list(zip(*self.vswr_out))
+        self._vswr_out_max = [max(vs[self._min_freq_index]), max(vs[mid]), max(vs[self._max_freq_index])]
+
+        self._phase_rmse_values = [self.phase_rmse[self._min_freq_index], self.phase_rmse[mid], self.phase_rmse[self._max_freq_index]]
+        self._s21_rmse_values = [self.s21_rmse[self._min_freq_index], self.s21_rmse[mid], self.s21_rmse[self._max_freq_index]]
+
+        vs = list(zip(*self.phase_err))
+        self._phase_err_max = [max(abs(v) for v in vs[self._min_freq_index]), max(abs(v) for v in vs[mid]), max(abs(v) for v in vs[self._max_freq_index])]
+
+        vs = list(zip(*self.s21_err))
+        self._s21_err_max = [max(abs(v) for v in vs[self._min_freq_index]), max(abs(v) for v in vs[mid]), max(abs(v) for v in vs[self._max_freq_index])]
+
+    def _cal_s21_worst_loss(self):
+        min_index = _find_freq_index(self._freqs, self._secondaryParams['Fborder1'])
+        max_index = _find_freq_index(self._freqs, self._secondaryParams['Fborder2'])
+
+        # min_index = 0
+        # max_index = len(self._freqs) - 1
+
+        level = self._secondaryParams['kp']
+        mins = [min(vals) for vals in zip(*self._s21s)]
+        res = itertools.groupby(mins, key=lambda x: x > level)
+        res = [list(ls) for val, ls in res if val]
+        if not res:
+            self._kp_freq_min = 'n/a'
+            self._kp_freq_max = 'n/a'
+            return
+        elif len(res) != len(self._freqs):
+            max_size = max(len(el) for el in res)
+            res = list(filter(lambda x: len(x) == max_size, res))[0]
+            min_index = mins.index(res[0])
+            max_index = mins.index(res[-1])
+        self._kp_freq_min = round(self._freqs[min_index] / 1_000_000_000, 2)
+        self._kp_freq_max = round(self._freqs[max_index] / 1_000_000_000, 2)
+
+    def _load_ideal(self):
+        print(f'reading adjust set from: {self.adjust_set}/')
+        for i in range(64):
+            if self.only_main_states and i not in self.main_states:
+                continue
+            with open(f'{self.adjust_set}/s{i}.s2p', mode='rt', encoding='utf-8') as f:
+                fs = []
+                s11dbs = []
+                s11degs = []
+                s21dbs = []
+                s21degs = []
+                s12dbs = []
+                s12degs = []
+                s22dbs = []
+                s22degs = []
+
+                for line in list(f.readlines())[5:]:
+                    res = map(float, line.strip().split())
+                    frq, s11db, s11deg, s21db, s21deg, s12db, s12deg, s22db, s22deg = res
+                    fs.append(frq)
+                    s11dbs.append(s11db)
+                    s11degs.append(s11deg)
+                    s21dbs.append(s21db)
+                    s21degs.append(s21deg)
+                    s12dbs.append(s12db)
+                    s12degs.append(s12deg)
+                    s22dbs.append(s22db)
+                    s22degs.append(s22deg)
+
+            self._s11s.append(s11dbs)
+            self._s21s.append(s21dbs)
+            self._s21s_ph.append(s21degs)
+            self._s22s.append(s22dbs)
+
+        self._freqs = fs
+        self._process()
+
+    @property
+    def raw_data(self):
+        return True
+
+    @raw_data.setter
+    def raw_data(self, args):
+        print('process result')
+        self._init()
+
+        points = int(args[0])
+        s2p = list(args[1])
+        self._ideal_phase = list(args[2])
+        self._secondaryParams = dict(args[3])
+        self._current = float(args[4])
+
+        if self.adjust:
+            self._load_ideal()
+            return
+
+        for pars in s2p:
+            for i in range(9):
+                array = pars[i * points: i * points + points]
+                if i == 0:
+                    self._freqs = array
+                elif i == 1:
+                    self._s11s.append(array)
+                elif i == 3:
+                    self._s21s.append(array)
+                elif i == 4:
+                    self._s21s_ph.append(array)
+                elif i == 7:
+                    self._s22s.append(array)
+        self._process()
+
+    @property
+    def freqs(self):
+        return self._freqs
+
+    @property
+    def s21(self):
+        return self._s21s
+
+    @property
+    def vswr_in(self):
+        return self._vswr_in
+
+    @property
+    def vswr_out(self):
+        return self._vswr_out
+
+    @property
+    def phase(self):
+        return self._s21s_ph
+
+    @property
+    def phase_err(self):
+        return self._s21s_ph_err
+
+    @property
+    def phase_rmse(self):
+        return self._s21s_ph_rmse
+
+    @property
+    def s21_err(self):
+        return self._s21s_err
+
+    @property
+    def s21_rmse(self):
+        return self._s21s_rmse
+
+    @property
+    def misc(self):
+        return self._misc
+
+    @property
+    def adjust_set(self):
+        return self._adjust_dir
+
+    @adjust_set.setter
+    def adjust_set(self, value):
+        self._adjust_dir = self.adjust_dirs[value]
+
+    @property
+    def stats(self):
+        low = self._min_freq_index
+        high = self._max_freq_index
+        mid = low + (high - low) // 2
+        f1 = round(self.freqs[low] / 1_000_000_000, 2)
+        f2 = round(self.freqs[mid] / 1_000_000_000, 2)
+        f3 = round(self.freqs[high] / 1_000_000_000, 2)
+
+        kp_freq_min = f'{self._kp_freq_min:.02f} ГГц' if self._kp_freq_min != 'n/a' else 'n/a'
+        kp_freq_max = f'{self._kp_freq_max:.02f} ГГц' if self._kp_freq_max != 'n/a' else 'n/a'
+
+        curr = self._current * 1_000
+
+        return f'''Потребление тока:
+{curr} мА при 5.25В
+
+Потери, минимум:
+{self._s21_mins[0]:.02f} дБ на {f1} ГГц
+{self._s21_mins[1]:.02f} дБ на {f2} ГГц
+{self._s21_mins[2]:.02f} дБ на {f3} ГГц
+
+КСВ вх, макс:
+{self._vswr_in_max[0]:.02f} на {f1} ГГц
+{self._vswr_in_max[1]:.02f} на {f2} ГГц
+{self._vswr_in_max[2]:.02f} на {f3} ГГц
+
+КСВ вых, макс:
+{self._vswr_out_max[0]:.02f} на {f1} ГГц
+{self._vswr_out_max[1]:.02f} на {f2} ГГц
+{self._vswr_out_max[2]:.02f} на {f3} ГГц
+
+φ, ошибка:
+{self._phase_err_max[0]:.02f} град на {f1} ГГц
+{self._phase_err_max[1]:.02f} град на {f2} ГГц
+{self._phase_err_max[2]:.02f} град на {f3} ГГц
+
+φ, СКО:
+{self._phase_rmse_values[0]:.02f} град на {f1} ГГц
+{self._phase_rmse_values[1]:.02f} град на {f2} ГГц
+{self._phase_rmse_values[2]:.02f} град на {f3} ГГц
+
+Потери, ошибка:
+{self._s21_err_max[0]:.02f} дБ на {f1} ГГц
+{self._s21_err_max[1]:.02f} дБ на {f2} ГГц
+{self._s21_err_max[2]:.02f} дБ на {f3} ГГц
+
+Потери, СКО:
+{self._s21_rmse_values[0]:.02f} дБ на {f1} ГГц
+{self._s21_rmse_values[1]:.02f} дБ на {f2} ГГц
+{self._s21_rmse_values[2]:.02f} дБ на {f3} ГГц
+
+Нижняя граница РЧ, Fн:
+{kp_freq_min}
+
+Верхняя граница РЧ, Fв:
+{kp_freq_max}
+'''
